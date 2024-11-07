2e490d896e6aa50f6ebd158a05629e60007d73ea
Danylo Shlapak <128870529+Born2Flex@users.noreply.github.com>
2024-11-07 23:55:42 +0200

    Add template project (#2)

---
+++ interview-app/src/test/java/ua/edu/ukma/interviewapp/InterviewAppApplicationTests.java
@@ -0,0 +1,14 @@
+package ua.edu.ukma.interviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Context loaded");
+    }
+
+}
