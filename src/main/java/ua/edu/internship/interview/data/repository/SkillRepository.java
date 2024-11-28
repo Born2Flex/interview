7f605920f5c2cbde90f0107b54c23b1e7cc2895e
Danylo Shlapak <geografy2314@gmail.com>
2024-11-28 15:05:05 +0200

    INT-26 Refactor mongo query

--- src/main/java/ua/edu/internship/interview/data/repository/SkillRepository.java
+++ src/main/java/ua/edu/internship/interview/data/repository/SkillRepository.java
@@ -1,7 +1,48 @@
 package ua.edu.internship.interview.data.repository;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ua.edu.internship.interview.data.entity.SkillEntity;
+import ua.edu.internship.interview.service.dto.SkillTreeDto;
+
+import java.util.List;
 
 public interface SkillRepository extends MongoRepository<SkillEntity, String> {
+    @Aggregation(pipeline = {
+            """
+            {
+              $match: {
+                  parentId: null
+              }
+            }
+            """,
+            """
+            {
+              $lookup: {
+                from: "skills",
+                  let: {
+                    parentId: "$_id"
+                  },
+                  pipeline: [
+                    {
+                      $match: {
+                        $expr: {
+                          $eq: ["$$parentId", "$parentId"]
+                        }
+                      }
+                    },
+                    {
+                      $lookup: {
+                        from: "skills",
+                        localField: "_id",
+                        foreignField: "parentId",
+                        as: "children"
+                      }
+                    }
+                  ],
+                as: "children"
+              }
+            }
+            """})
+    List<SkillTreeDto> findSkillTree();
 }
