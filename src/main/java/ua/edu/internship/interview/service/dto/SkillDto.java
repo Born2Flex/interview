4b6c2cbb5fd3c1698c31b3a4e0d9eae2ac65dc71
Danylo Shlapak <geografy2314@gmail.com>
2024-12-01 00:02:49 +0200

    INT-31 Add user skills and questions API

--- src/main/java/ua/edu/internship/interview/service/dto/SkillDto.java
+++
@@ -1,9 +0,0 @@
-package ua.edu.internship.interview.service.dto;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class SkillDto extends SkillBaseDto {
-}
