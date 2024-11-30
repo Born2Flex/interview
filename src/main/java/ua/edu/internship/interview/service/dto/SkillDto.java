e773e64f18b6f334ed1fecb9ac02b4143f4666b5
Danylo Shlapak <geografy2314@gmail.com>
2024-11-30 23:55:03 +0200

    INT-28 Complete skill api, add tests

---
+++ src/main/java/ua/edu/internship/interview/service/dto/SkillDto.java
@@ -0,0 +1,9 @@
+package ua.edu.internship.interview.service.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class SkillDto extends SkillBaseDto {
+}
