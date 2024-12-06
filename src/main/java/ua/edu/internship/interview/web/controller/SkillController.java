ffe24e695157fafdd8aab0cdaa89893ea4a0dcf2
Danylo Shlapak <geografy2314@gmail.com>
2024-12-06 13:39:57 +0200

    INT-28 Update documentation, add docker-compose.yaml

--- src/main/java/ua/edu/internship/interview/web/controller/SkillController.java
+++ src/main/java/ua/edu/internship/interview/web/controller/SkillController.java
@@ -1,5 +1,10 @@
 package ua.edu.internship.interview.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.edu.internship.interview.service.business.SkillService;
 import ua.edu.internship.interview.service.dto.skill.SkillTreeDto;
+import ua.edu.internship.interview.web.handler.ErrorResponse;
 import java.util.List;
 
 @Tag(name = "Skills", description = "Skills management endpoints")
@@ -18,11 +24,18 @@ public class SkillController {
     private final SkillService skillService;
 
     @GetMapping
+    @Operation(summary = "Get all skills")
+    @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(
+            schema = @Schema(implementation = SkillTreeDto.class)), mediaType = "application/json")})
     public List<SkillTreeDto> getSkills() {
         return skillService.getAllSkillTrees();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get skills by root ID")
+    @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = SkillTreeDto.class),
+            mediaType = "application/json")})
+    @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})
     public SkillTreeDto getSkillById(@PathVariable String id) {
         return skillService.getSkillTreeById(id);
     }
